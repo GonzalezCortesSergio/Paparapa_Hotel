43e4c91b82a8583ac174458feed283cc877b9e7b
Sergio González Cortés <gonzalez.coser23@triana.salesianos.edu>
2024-05-30 14:14:11 +0200

    Second commit

---
+++ src/main/java/com/salesianostriana/dam/hotelpaparapa/HotelpaparapaApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.hotelpaparapa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelpaparapaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelpaparapaApplication.class, args);
+	}
+
+}
