e91fbab28f91307ba1242f8489aae2d021bc87db
Sergio González Cortés <gonzalez.coser23@triana.salesianos.edu>
2024-05-31 15:18:10 +0200

    Consulta de RepositorioReserva mal hecho pero weno

--- src/main/java/com/salesianostriana/dam/hotelpaparapa/repository/RepositorioReserva.java
+++ src/main/java/com/salesianostriana/dam/hotelpaparapa/repository/RepositorioReserva.java
@@ -3,6 +3,18 @@ package com.salesianostriana.dam.hotelpaparapa.repository;
 import com.salesianostriana.dam.hotelpaparapa.model.Reserva;
 import com.salesianostriana.dam.hotelpaparapa.model.ReservaPK;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 public interface RepositorioReserva extends JpaRepository<Reserva, ReservaPK> {
+
+    @Query("""
+            SELECT r
+            FROM Reserva r
+            WHERE r.id = :idResera AND r.fechaLlegada = :fechaEntrada
+            """)
+    Optional<Reserva> reservaFechaEntrada(@Param("fechaEntrada")LocalDate fechaEntrada, @Param("idReserva") long idReserva);
 }
